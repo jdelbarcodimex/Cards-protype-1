9c60a97196a1576dc68a11eb89dcb0292eee8803
Jorge Del Barco Garza <jorge.delbarco@dimex.mx>
2022-10-11 10:54:07 -0500

    setup commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+
+<script setup>
+
+import FiboCard from "./components/FiboCard.vue";
+
+import { ref, reactive } from "vue";
+
+const cardValues = ref([1, 2, 3, 5, 8, 13, 21, 34]);
+
+const playedCards = ref([]);
+
+function addCard(card) {
+
+  playedCards.append(card)
+  console.log(playedCards)
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="centro-cartas" >
+      <FiboCard v-for="card in playedCards" v-bind:valor="card"/>
+    </div>
+
+    <div class="maso-cartas" @cardPlayed="addCard(card)">
+
+      <FiboCard v-for="card in cardValues" v-bind:valor="card" v-bind:card-in-deck="true" />
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  flex-direction: column;
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+}
+
+.centro-cartas {
+  width: 100%;
+  background-color: beige;
+  height: 70%;
+  display: flex;
+  justify-content: space-around;
+}
+
+.maso-cartas {
+  display: flex;
+  width: 100%;
+  height: 30%;
+  background-color: cadetblue;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ src/components/FiboCard.vue
@@ -0,0 +1,48 @@
+
+<template>
+
+    <div v-if="state.cardInDeck != false" class="card" @click="playCard" >
+        <p>{{props.valor}}</p>
+    </div>
+
+
+</template>
+
+<script setup>
+
+import { reactive } from 'vue';
+
+const state = reactive({cardInDeck: true});
+const emit = defineEmits(['cardPlayed'])
+
+function playCard() {
+    state.cardInDeck = !state.cardInDeck;
+    emit('cardPlayed', valor);
+    console.log(state.cardInDeck);
+}
+ 
+    const props = defineProps({
+        valor: Number,
+        cardInDeck: Boolean,
+    })
+
+    
+
+</script>
+
+<style>
+
+    .card {
+        padding: 12px 24px;
+        background-color: brown;
+        width: 50px;
+        height: fit-content;
+        border-radius: 10px;
+    }
+
+    div > p {
+        color: white;
+        font-size: 36px;
+    }
+
+</style>
